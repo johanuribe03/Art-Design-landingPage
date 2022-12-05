118de7da7f5b5f7e74fd4d89b86d19c5a7ecaa83
johanuribe03 <johan.uribe@gmail.com>
2022-12-05 11:19:50 +0000

    Fixing class to className

--- src/js/component/hero.jsx
+++ src/js/component/hero.jsx
@@ -6,9 +6,9 @@ import React from "react";
 //create your first component
 const Hero = () => {
 	return (
-        <div class="hero-image">
-            <div class="hero-text">
-                <h1 style="font-size:50px">I am John Doe</h1>
+        <div className="hero-image">
+            <div className="hero-text">
+                <h1 style="font-size: 50px">I am John Doe</h1>
                 <p>And I'm a Photographer</p>
                 <button>Hire me</button>
             </div>
